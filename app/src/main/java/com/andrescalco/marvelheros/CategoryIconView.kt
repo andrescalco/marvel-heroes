918af533e756747dc199247f8b307e568dd0e5f8
André Scalco <andre.scalco@skipthedishes.ca>
2020-06-18 19:41:21 -0500

    master: Use synthetic instead of findById

--- app/src/main/java/com/andrescalco/marvelheros/CategoryIconView.kt
+++ app/src/main/java/com/andrescalco/marvelheros/CategoryIconView.kt
@@ -2,26 +2,17 @@ package com.andrescalco.marvelheros
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.andrescalco.marvelheros.R.id.categoryIcon
-import com.andrescalco.marvelheros.R.id.categoryView
 import com.andrescalco.marvelheros.R.styleable.*
+import kotlinx.android.synthetic.main.category_icon_view.view.*
 
 class CategoryIconView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
     init {
         inflate(context, R.layout.category_icon_view, this)
-
-        val categoryView = findViewById<ConstraintLayout>(categoryView)
-        val categoryIcon = findViewById<ImageView>(categoryIcon)
-
         val attributes = context.obtainStyledAttributes(attrs, CategoryIconView)
-
         categoryIcon.setImageDrawable(attributes.getDrawable(CategoryIconView_icon))
         categoryView.background = attributes.getDrawable(CategoryIconView_gradient)
         attributes.recycle()
-
     }
-
 }
