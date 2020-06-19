88b5a16fd7818d179950ffe9eb102431a0675586
André Scalco <andre.scalco@skipthedishes.ca>
2020-06-18 19:38:38 -0500

    master: Remove not nescessary array. Use ?.let

--- app/src/main/java/com/andrescalco/marvelheros/CustomAttributes.kt
+++ app/src/main/java/com/andrescalco/marvelheros/CustomAttributes.kt
@@ -3,12 +3,11 @@ package com.andrescalco.marvelheros
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-@BindingAdapter(value = ["setCharacters"])
+@BindingAdapter("setCharacters")
 fun RecyclerView.setCharacters(characters: List<Character>?) {
-    if (characters != null) {
+    characters?.let{
         val characterAdapter = CharacterAdapter()
         characterAdapter.submitList(characters)
-
         adapter = characterAdapter
     }
 }
